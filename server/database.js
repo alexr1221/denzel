56f045ac65d421a87db71d59667042580406869c
alexr1221 <alexr2030@gmail.com>
2020-03-23 20:39:28 +0100

    REST(enpoint): Added  /movies endpoint

--- server/database.js
+++ server/database.js
@@ -21,5 +21,25 @@ module.exports.populateDatabase = async docs => {
             }
         });
     });
-    
+};
+
+module.exports.getAllMovies = async() => {
+    return new Promise((success, failed) => {
+        mongo.connect("mongodb+srv://alex:mongom@ddenzel-pwqvc.azure.mongodb.net/", function (err, db) {
+            if (err) failed(err);
+            var dbo = db.db("ddenzel");
+            try {
+                console.log("trying find...");
+                dbo.collection("movies").find().toArray(function (err, res) {
+                    if (err) throw err;
+                    db.close();
+                    success(res);
+                });
+            }
+            catch (e) {
+                console.log(e);
+                failed(e)
+            }
+        });
+    });
 };
--- server/index.js
+++ server/index.js
@@ -21,21 +21,37 @@ app.get('/', (request, response) => {
 
 app.get('/movies/populate', async (request, response) => {
     var actor_id = request.query["id"];
-    var metascore = 72;
     console.log(actor_id);
 
 
     try {
         console.log(`📽️  fetching filmography of ${actor_id}...`);
         const movies = await imdb(actor_id);
-        const awesome = movies.filter(movie => movie.metascore >= metascore);
 
         var db_result_message = await database.populateDatabase(movies);
         //console.log(`🍿 ${movies.length} movies found.`);
         //      console.log(JSON.stringify(movies, null, 2));
         //    console.log(`🥇 ${awesome.length} awesome movies found.`);
         //    console.log(JSON.stringify(awesome, null, 2));
-        response.send("operation completed - " + db_result_message);
+        console.log("operation completed")
+        response.send(db_result_message);
+    } catch (e) {
+        console.error(e);
+        response.send("an error occured");
+    }
+});
+
+app.get('/movies', async (request, response) => {
+    var metascore = 70;
+
+    try {
+        console.log(`📽️  fetching random must-watch movie...`);
+        const movies = await database.getAllMovies();
+        const mustwatches = movies.filter(movie => movie.metascore >= metascore);
+        var random_index = Math.floor(Math.random() * mustwatches.length);
+        var rdm_mustwatch = mustwatches[random_index];
+        console.log("operation completed");
+        response.send(JSON.stringify(rdm_mustwatch, null, 2));
     } catch (e) {
         console.error(e);
         response.send("an error occured");
